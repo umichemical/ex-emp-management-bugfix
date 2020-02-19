43d8cc0d0b82f1b8a56536479b49cc0a53c7bf61
okadayuri <chemistory.12@gmail.com>
2020-02-19 17:30:26 +0900

    (2-1)ログイン後、従業員一覧と従業員詳細画面に「〇〇さん　こんにちは！」と表示するように変更

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -116,6 +116,7 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		session.setAttribute("administratorName", administrator.getName());
 		return "forward:/employee/showList";
 	}
 	
